72f36f811748bf7c958768560aa5ff04ad85e331
Myoungho Shin <myounghoshin84@gmail.com>
2026-05-29 08:11:18 -0700

    update the production url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://gpu-flight.github.io',
+  url: 'https://docs.gpuflight.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
